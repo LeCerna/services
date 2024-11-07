2f15b895f612ccfee108a8216690968129e15119
LeCerna <93564888+LeCerna@users.noreply.github.com>
2024-11-06 18:36:48 -0600

    Add files via upload

--- car.js
+++ car.js
@@ -1,16 +1,28 @@
 
-    let currentIndex = 0;
-    const items = document.querySelectorAll('.carousel-item');
-    const totalItems = items.length;
 
-    function showNext() {
-        currentIndex = (currentIndex + 1) % totalItems;
-        updateCarousel();
-    }
+const slider = document.querySelector('.section');
+const slides = document.querySelectorAll('.slider img');
+const sliderNav = document.querySelector('.slider-nav');
+let activeSlide = 0;
 
-    function updateCarousel() {
-        const newPosition = -currentIndex * 100;
-        document.querySelector('.carousel-inner').style.transform = `translateX(${newPosition}%)`;
-    }
+const createNavDots = () => {
+ slides.forEach((_, i) => {
+    const navDot = document.createElement('a');
+    navDot.setAttribute('href', `#slide-${i + 1}`);
+    navDot.setAttribute('class', `nav-dot-${i + 1}`);
+    sliderNav.appendChild(navDot);
+ });
+};
 
-    setInterval(showNext, 4000); // Change image every  seconds
+const autoplay = () => {
+ setInterval(() => {
+    slider.scrollTo({
+      left: slides[activeSlide].offsetLeft,
+      behavior: 'smooth',
+    });
+    activeSlide = (activeSlide + 1) % slides.length;
+ }, 5000); // 5000 milliseconds = 5 seconds
+};
+
+createNavDots();
+autoplay();
